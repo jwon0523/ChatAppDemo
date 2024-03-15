05cbc709265ef89cadcd38dea8df52602e28f533
JaewonLee <jwon0523@gmail.com>
2024-03-15 23:35:42 +0900

    Initial Commit

---
+++ ChatAppDemo/ChatAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemoApp.swift
+//  ChatAppDemo
+//
+//  Created by jaewon Lee on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
