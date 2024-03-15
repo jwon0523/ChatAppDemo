5fb2d249ca236a926de6dfd0de9a6f8081bcbfc7
jwon0523 <jwon010523@gmail.com>
2024-03-16 01:59:16 +0900

    Connected FireBase

--- ChatAppDemo/ContentView.swift
+++ ChatAppDemo/ContentView.swift
@@ -8,21 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    var messageArray = ["Hello!!", "How are you doing?", "Good!!!!"]
+    @StateObject var messagesManager = MessagesManager() 
     
     var body: some View {
         VStack {
             TitleRow()
             
             ScrollView {
-                ForEach(messageArray, id: \.self) { text in
-                    MessageBubble(
-                        message: Message(
-                            id: "1",
-                            text: text,
-                            received: true,
-                            timestamp: Date()
-                        ))
+                ForEach(messagesManager.messages, id: \.id) { message in
+                    MessageBubble(message: message)
                 }
             }
             .padding(.top, 10)
---
+++ ChatAppDemo/MessagesManager.swift
@@ -0,0 +1,39 @@
+//
+//  MessagesManager.swift
+//  ChatAppDemo
+//
+//  Created by jaewon Lee on 3/16/24.
+//
+
+import Foundation
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+class MessagesManager: ObservableObject {
+    @Published private(set) var messages: [Message] = []
+    let db = Firestore.firestore()
+    
+    init() {
+        getMessage()
+    }
+    
+    func getMessage() {
+        db.collection("messages").addSnapshotListener { querySnapshot, error in
+            guard let documents = querySnapshot?.documents else {
+                print("Error fetching documents: \(String(describing: error))")
+                return
+            }
+            
+            self.messages = documents.compactMap({ document -> Message? in
+                do {
+                    return try document.data(as: Message.self)
+                } catch {
+                    print("Error decoding document into Message: \(error)")
+                    return nil
+                }
+            })
+            
+            self.messages.sort { $0.timestamp < $1.timestamp }
+        }
+    }
+}
